8c59259cd9b28295deae5ea2962cefbc807b6340
admin <admin@admins-MacBook-Pro.local>
2017-12-24 23:24:00 +0000

    connects the service, modules and Weather API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WeatherService } from './weather.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Weather Chart';
+  constructor(private _weather: WeatherService) {}
+
+  ngOnInit() {
+    this._weather.dailyForecast()
+        .subscribe(response => {
+          console.log(response);
+        });
+  }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  dailyForecast() {
+    return  this._http.get("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22")
+        .map(result => result);
+  }
 
 }
