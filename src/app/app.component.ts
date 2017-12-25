e436e59f1e6fd86a7e9ed77a4c9dbdae27acf112
admin <admin@admins-MacBook-Pro.local>
2017-12-25 00:08:49 +0000

    adds chart infrastructure and basic settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WeatherService } from './weather.service';
+import { Chart } from 'chart.js';
 
 
 @Component({
@@ -9,12 +10,55 @@ import { WeatherService } from './weather.service';
 })
 export class AppComponent {
   title = 'Weather Chart';
+  chart = [];
   constructor(private _weather: WeatherService) {}
 
   ngOnInit() {
     this._weather.dailyForecast()
         .subscribe(response => {
-          console.log(response);
-        });
+          let temp_max = response['list'].map(res => res.main.temp_max);
+          let temp_min = response['list'].map(res => res.main.temp_min);
+          let alldates = response['list'].map(res => res.dt);
+
+          let weatherDates = [];
+          alldates.forEach((res) => {
+            let jsdate = new Date(res * 1000);
+            weatherDates.push(jsdate.toLocaleTimeString('en', {year: 'numeric', month: 'short', day: 'numeric'}));
+          })
+
+            // Chart options
+            this.chart = new Chart('canvas', {
+              type: 'line',
+              data: {
+                labels : weatherDates,
+                datasets: [
+                  {
+                    data: temp_max,
+                    borderColor: '#3cba9f',
+                    fill: false
+                  },
+                  {
+                    data: temp_min,
+                    borderColor: '#ffcc00',
+                    fill: false
+                  },
+                ]
+              },
+              options: {
+                legend: {
+                  display: false
+                },
+                scales: {
+                  xAxes: [{
+                    display: true
+                  }],
+                  yAxes: [{
+                    display: true
+                  }]
+                }
+              }
+            })
+
+        })
   }
 }
